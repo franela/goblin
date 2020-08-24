cef7b20e319cf9833ac725ddcda1d11ba622d12b
cyprien <cyprien.augier@protonmail.com>
2020-08-24 18:59:06 +0200

    Added separate tests for race conidtions

---
+++ race_test.go
@@ -0,0 +1,46 @@
+// +build race
+
+package goblin
+
+import (
+	"testing"
+	"time"
+)
+
+func TestG_It_AsyncDone_Race(t *testing.T) {
+	fakeTest := testing.T{}
+	g := Goblin(&fakeTest)
+
+	g.Describe("Async test", func() {
+		g.It("Should not create a data race if done() is called multiple times", func(done Done) {
+			go func() {
+				time.Sleep(100 * time.Millisecond)
+				done()
+				done()
+			}()
+		})
+	})
+}
+
+func TestG_It_Fail_Race(t *testing.T) {
+	g := Goblin(new(testing.T))
+
+	g.Describe("Synchronous test", func() {
+		g.It("Should not create a data race on fail", func() {
+			g.Fail("Failed")
+		})
+	})
+}
+
+func TestG_It_Assert_Race(t *testing.T) {
+	g := Goblin(new(testing.T))
+	g.SetReporter(Reporter(new(FakeReporter)))
+
+	g.Describe("Should not create a data race", func() {
+		g.It("Should fail", func() {
+			g.Assert(0).Equal(1)
+		})
+		g.It("Should pass", func() {
+		})
+	})
+}
